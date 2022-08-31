49083cd68392f4f2f1c2a9408d187ba43b0863b5
Gabridg <gabri99.del@gmail.com>
2022-08-31 17:03:56 +0200

    finished logic

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php 
 $FAQs = [
-    'Come Stai?' => [
-        'text' = 'bene,grazie'
-    ],
-    [
-        'text' = 'malissimo, mi manca Anastasia'
-    ],
-'Ti piace il lavoro?' => [
-        'text' = ' si, è entusiasmante!'
-    ],
-    [
-        'text' = 'no, è estenuante!'
-    ]
+    'Come Stai?' => 'bene,grazie',
+'Ti piace il lavoro?' => ' si, è entusiasmante!'
+];
+$ask = array_keys($FAQs);
 
-]
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,8 +18,7 @@ $FAQs = [
 <body>
     <?php foreach($FAQs as $ask => $FAQ) : ?>
         <h2><?= $ask ?></h2>
-        <p><?=  $FAQ['text'] ?></p>
+        <p><?= $FAQ ?></p>
     <?php endforeach; ?>
 </body>
 </html>
-
